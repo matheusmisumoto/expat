d6d529a0ff1aa35269662fc666de491a71bf2c39
Matheus Misumoto <49180175+matheusmisumoto@users.noreply.github.com>
2020-11-06 16:48:08 -0300

    First commit

---
+++ src/main/java/br/com/expat/model/Usuario.java
@@ -0,0 +1,99 @@
+package br.com.expat.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name = "tb_usuarios")
+@Entity
+public class Usuario implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "cd_usuario", length = 20)
+	private long id;
+	
+	@Column(name = "ds_email", length = 60)
+	private String email;
+	
+	@Column(name = "pw_usuario", length = 60)
+	private String password;
+	
+	@Column(name = "nm_usuario", length = 60)
+	private String nome;
+	
+	public Usuario() {	
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", email=" + email + ", password=" + password + ", nome=" + nome + "]";
+	}
+	
+	
+
+}
---
+++ src/main/java/br/com/expat/repository/CustoRepository.java
@@ -0,0 +1,11 @@
+package br.com.expat.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.expat.model.Custo;
+
+@Repository
+public interface CustoRepository extends JpaRepository<Custo, Long> {
+
+}
