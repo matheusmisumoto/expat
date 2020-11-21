7e66fe12a66a2cc86a906db72cc61c607316427e
Matheus Misumoto <49180175+matheusmisumoto@users.noreply.github.com>
2020-11-21 08:25:17 -0300

    Fase de Testes

--- src/main/java/br/com/expat/config/SecurityConfig.java
+++ src/main/java/br/com/expat/config/SecurityConfig.java
@@ -40,6 +40,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	private static final String[] PUBLIC_MATCHERS_POST = {
 			"/expat/usuario/**",
+			"/expat/custo/**",
+			"/expat/cidade/**" 
 			};
 	
 	@Override
